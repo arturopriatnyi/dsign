c43378f09fa6ca5966caaa515905a4bd3fc637e2
artur.opriatnyi <imarrche@gmail.com>
2022-02-18 06:41:13 +0200

    Add data signing

--- dsign.go
+++ dsign.go
@@ -1,14 +1,38 @@
 // Package dsign is a package for working with digital signatures.
 package dsign
 
-import "crypto/ed25519"
+import (
+	"bytes"
+	"crypto/ed25519"
+	"crypto/sha256"
+	"errors"
+	"io"
+)
 
 const (
+	// SignatureSize is the size of signatures in bytes.
+	SignatureSize = 64
 	// PrivateKeySize is the size of private keys in bytes.
 	PrivateKeySize = 64
 	// PublicKeySize is the size of public keys in bytes.
 	PublicKeySize = 32
 )
+
+// ErrInvalidKeySize is error that is returned when key has invalid size.
+var ErrInvalidKeySize = errors.New("invalid key size")
+
+// Signature is a data hash signed with PrivateKey.
+type Signature []byte
+
+// Size returns the size of Signature.
+func (s Signature) Size() int {
+	return len(s)
+}
+
+// Equals checks if two signatures are equal.
+func (s Signature) Equals(ss Signature) bool {
+	return bytes.Equal(s, ss)
+}
 
 // PrivateKey is a private key used for signing.
 type PrivateKey []byte
@@ -16,6 +40,21 @@ type PrivateKey []byte
 // Size returns the size of PrivateKey.
 func (k PrivateKey) Size() int {
 	return len(k)
+}
+
+// Sign signs data hash with PrivateKey.
+func (k PrivateKey) Sign(data io.Reader) (Signature, error) {
+	if k.Size() != PrivateKeySize {
+		return nil, ErrInvalidKeySize
+	}
+
+	h := sha256.New()
+	if _, err := io.Copy(h, data); err != nil {
+		return nil, err
+	}
+
+	// ignoring panic here because we've already checked PrivateKey size
+	return ed25519.Sign(ed25519.PrivateKey(k), h.Sum(nil)), nil
 }
 
 // PublicKey is a public key used for signature verification.
--- dsign_test.go
+++ dsign_test.go
@@ -1,6 +1,71 @@
 package dsign
 
-import "testing"
+import (
+	"errors"
+	"io"
+	"strings"
+	"testing"
+)
+
+var (
+	testData      = "data"
+	testSignature = Signature{
+		250, 110, 27, 175, 217, 121, 115, 118, 254, 207,
+		35, 116, 88, 86, 84, 171, 139, 35, 240, 244,
+		114, 106, 27, 155, 111, 140, 239, 220, 98, 63,
+		132, 88, 79, 178, 105, 217, 253, 81, 172, 19,
+		11, 131, 83, 198, 73, 246, 165, 225, 156, 217,
+		62, 226, 26, 206, 212, 38, 212, 108, 8, 207,
+		102, 140, 243, 9,
+	}
+	testPrivateKey = PrivateKey{
+		229, 15, 252, 40, 59, 140, 227, 35, 166, 181,
+		199, 33, 13, 105, 157, 9, 118, 58, 33, 219,
+		94, 84, 229, 161, 162, 8, 158, 202, 40, 203,
+		48, 192, 214, 218, 41, 143, 49, 88, 77, 203,
+		255, 20, 45, 70, 126, 18, 67, 110, 35, 196,
+		33, 253, 3, 86, 126, 4, 130, 247, 84, 118,
+		21, 184, 141, 199,
+	}
+)
+
+func TestPrivateKey_Sign(t *testing.T) {
+	testcases := map[string]struct {
+		privateKey   PrivateKey
+		data         io.Reader
+		expSignature Signature
+		expError     error
+	}{
+		"data is signed": {
+			privateKey:   testPrivateKey,
+			data:         strings.NewReader(testData),
+			expSignature: testSignature,
+			expError:     nil,
+		},
+		"invalid private key size": {
+			privateKey:   PrivateKey{},
+			data:         strings.NewReader(testData),
+			expSignature: nil,
+			expError:     ErrInvalidKeySize,
+		},
+	}
+
+	for name, tc := range testcases {
+		t.Run(name, func(t *testing.T) {
+			s, err := tc.privateKey.Sign(tc.data)
+
+			if !errors.Is(err, tc.expError) {
+				t.Errorf("error: %v, expected: %v", err, tc.expError)
+			}
+			if tc.expError == nil && s.Size() != SignatureSize {
+				t.Errorf("signature size: %v, expected: %v", s.Size(), SignatureSize)
+			}
+			if tc.expError == nil && !s.Equals(tc.expSignature) {
+				t.Errorf("signature: %x, expected: %x", s, tc.expSignature)
+			}
+		})
+	}
+}
 
 func TestGenerateKeys(t *testing.T) {
 	privateKey, publicKey, err := GenerateKeys()
